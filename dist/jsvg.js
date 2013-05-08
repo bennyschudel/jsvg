c7c1596c1e3f652d0a496f289c24ec6f2c2fca6b
Benny Schudel <bennyschudel@gmail.com>
2013-05-08 16:07:05 +0200

    Added demo

--- dist/jsvg.js
+++ dist/jsvg.js
@@ -20,12 +20,12 @@ $sa.SVGLoader = (function($, undefined) {
 			$.extend(true, opt, options);
 		};
 
-		self.load = function() { // *options
+		self.load = function(arg0) {
 			var _opt = $.extend({
 					url: '',
 					name: null,
 					cache: opt.cache
-				}, arguments[0]),
+				}, arg0),
 
 				name = _opt.name;
 
@@ -41,7 +41,7 @@ $sa.SVGLoader = (function($, undefined) {
 		self.getAsset = function(name) {
 			if (name instanceof $sa.SVGAsset) { return name; }
 
-			if (!name in assets) {
+			if (!(name in assets)) {
 				throw new Error("Could not find asset: "+name);
 			}
 
@@ -55,18 +55,11 @@ $sa.SVGLoader = (function($, undefined) {
 		self.init();
 	};
 
-	_static = {
-		name: _name,
-		instance: function(options) {
-			if (!_instance) {
-				_instance = new SVGLoader(options);
-			}
+	if (!_instance) {
+		_instance = new SVGLoader();
+	}
+	return _instance;
 
-			return _instance;
-		}
-	};
-
-	return _static;
 })(jQuery);
 $sa.SVGAsset = (function($, undefined) {
 	var _ns = 'sa',
